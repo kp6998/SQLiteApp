b204790af73d4ed018186fb6a36a2b99b4d4011e
Prakash <98660007+prakash-kathiresan@users.noreply.github.com>
2024-02-21 20:45:44 +0530

    Add project files.

---
+++ SQLiteApp/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using Microsoft.Data.Sqlite;
+
+namespace SQLiteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder();
+
+            //Use DB in project directory.  If it does not exist, create it:
+            connectionStringBuilder.DataSource = "./SqliteDB.db";
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                //Create a table (drop if already exists first):
+                var delTableCmd = connection.CreateCommand();
+                delTableCmd.CommandText = "DROP TABLE IF EXISTS favorite_beers";
+                delTableCmd.ExecuteNonQuery();
+
+                var createTableCmd = connection.CreateCommand();
+                createTableCmd.CommandText = "CREATE TABLE favorite_beers(name VARCHAR(50))";
+                createTableCmd.ExecuteNonQuery();
+
+                //Seed some data:
+                using (var transaction = connection.BeginTransaction())
+                {
+                    var insertCmd = connection.CreateCommand();
+
+                    insertCmd.CommandText = "INSERT INTO favorite_beers VALUES('LAGUNITAS IPA')";
+                    insertCmd.ExecuteNonQuery();
+
+                    insertCmd.CommandText = "INSERT INTO favorite_beers VALUES('JAI ALAI IPA')";
+                    insertCmd.ExecuteNonQuery();
+
+                    insertCmd.CommandText = "INSERT INTO favorite_beers VALUES('RANGER IPA')";
+                    insertCmd.ExecuteNonQuery();
+
+                    transaction.Commit();
+                }
+
+                //Read the newly inserted data:
+                var selectCmd = connection.CreateCommand();
+                selectCmd.CommandText = "SELECT name FROM favorite_beers";
+
+                using (var reader = selectCmd.ExecuteReader())
+                {
+                    int i = 1;
+                    while (reader.Read())
+                    {
+                        var message = reader.GetString(0);
+                        Console.WriteLine(message + i++);
+                    }
+                }
+
+            }
+        }
+    }
+}
